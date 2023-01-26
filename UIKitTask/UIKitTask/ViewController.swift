9a652fafb09d4b923b631e23d4389f45664d1473
Alex Schnapps <6kwalitet@gmail.com>
2023-01-27 00:59:08 +0400

    add slider

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -11,13 +11,22 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player = AVAudioPlayer()
+    var slider = UISlider()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        slider.frame = CGRect(x: 0, y: 0, width: 23, height: 200)
+        slider.center = view.center
+        slider.minimumValue = 0.0
+        slider.maximumValue = 100.0
+        view.addSubview(slider)
+        
+        
         do {
             if let audiopatch = Bundle.main.path(forResource: "Mit dem Wind", ofType: "mp3") {
                 try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audiopatch))
+                slider.maximumValue = Float(player.duration)
             }
         } catch {
             print("Error")
@@ -25,11 +34,14 @@ class ViewController: UIViewController {
         self.player.play()
     }
     
+    //MARK: - Action
     @IBAction func playButton(_ sender: UIButton) {
+        player.play()
     }
     
     
     @IBAction func stopButton(_ sender: UIButton) {
+        player.stop()
     }
 }
 
