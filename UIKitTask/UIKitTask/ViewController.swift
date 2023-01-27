958a126cd734ff4a9130522d58447d2259830b4c
Alex Schnapps <6kwalitet@gmail.com>
2023-01-27 18:05:52 +0400

    one slider complit

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    //MARK: - outlet
+    
+    @IBOutlet weak var sliderBoard: UIView!
     
     var player = AVAudioPlayer()
     var slider = UISlider()
@@ -16,12 +19,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        slider.frame = CGRect(x: 0, y: 0, width: 23, height: 200)
-        slider.center = view.center
+        //MARK: - Slider
+        slider.frame = CGRect(x: 0, y: 0, width: 200, height: 20)
+        slider.center = self.view.center
         slider.minimumValue = 0.0
-        slider.maximumValue = 100.0
+        slider.maximumValue = 200.0
+        slider.tintColor = .green
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .yellow
+        slider.isContinuous = false
         view.addSubview(slider)
-        
+        slider.addTarget(self, action: #selector(changeSlider), for: .valueChanged)
         
         do {
             if let audiopatch = Bundle.main.path(forResource: "Mit dem Wind", ofType: "mp3") {
@@ -34,6 +42,12 @@ class ViewController: UIViewController {
         self.player.play()
     }
     
+    @objc func changeSlider(sender: UISlider) {
+        if sender == slider {
+            self.player.currentTime = TimeInterval(sender.value)
+        }
+    }
+    
     //MARK: - Action
     @IBAction func playButton(_ sender: UIButton) {
         player.play()
@@ -43,5 +57,11 @@ class ViewController: UIViewController {
     @IBAction func stopButton(_ sender: UIButton) {
         player.stop()
     }
+    
+    
+    @IBAction func sliderAction(_ sender: Any) {
+      //  self.player.volume = self.sliderBoard.
+    }
+    
 }
 
