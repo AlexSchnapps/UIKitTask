7e45ca862504b1e6e65300fb188bf97aec36b07a
Alex Schnapps <6kwalitet@gmail.com>
2023-01-28 13:43:08 +0400

    create segmentedControl

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var segmentControl = UISegmentedControl()
+    var menuArray = ["One", "Two", "Three"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // MARK: - Create segmentControl
+        segmentControl = UISegmentedControl(items: self.menuArray)
+        segmentControl.frame = CGRect(x: 0, y: 0, width: 250, height: 100)
+        segmentControl.center = self.view.center
+        view.addSubview(segmentControl)
     }
 
 
