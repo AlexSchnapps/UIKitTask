1fa1d70ccc8c6254fd372f5dbf3783ce63d47f84
Alex Schnapps <6kwalitet@gmail.com>
2023-01-28 14:48:47 +0400

    add image

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -8,19 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var segmentControl = UISegmentedControl()
     var menuArray = ["One", "Two", "Three"]
+    let imageArray = [
+        UIImage(named: "Anubys.jpg"),
+        UIImage(named: "Bhastet.jpg"),
+        UIImage(named: "Ghor.jpg")
+    ]
+    var imageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // MARK: - Create segmentControl
         segmentControl = UISegmentedControl(items: self.menuArray)
-        segmentControl.frame = CGRect(x: 0, y: 0, width: 250, height: 100)
-        segmentControl.center = self.view.center
-        view.addSubview(segmentControl)
+        segmentControl.frame = CGRect(x: 60, y: 500, width: 250, height: 100)
+        
+        self.view.addSubview(segmentControl)
+        
+        //MARK: - Image
+        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.center = view.center
+        imageView.image = imageArray[0]
+        self.view.addSubview(imageView)
     }
-
-
+    
+    
 }
 
