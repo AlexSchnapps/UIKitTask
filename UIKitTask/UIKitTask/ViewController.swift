e292a42debb83d17cc5596c3ae4ee33887af1b8e
Alex Schnapps <6kwalitet@gmail.com>
2023-02-14 00:01:58 +0400

    create secondVC

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var displaySecondButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = "FirstVC"
+    
+        self.displaySecondButton = UIButton(type: .system)
+        self.displaySecondButton.setTitle("SecondVC", for: .normal)
+        self.displaySecondButton.sizeToFit()
+        self.displaySecondButton.center = self.view.center
+        self.displaySecondButton.addTarget(self, action: #selector(performDisplaySecondVC), for: .touchUpInside)
+        self.view.addSubview(self.displaySecondButton)
     }
 
+    @objc func performDisplaySecondVC(parametrSender: Any) {
+        let secondVC = SecondViewController()
+        self.navigationController?.pushViewController(secondVC, animated: true)
+    }
 
 }
 
