30e798dc6b1fc33b52da5eae23eb0772f4fdac99
Alex Schnapps <6kwalitet@gmail.com>
2023-02-15 18:38:41 +0400

    lesson complited

--- UIKitTask/UIKitTask/SecondViewController.swift
+++ UIKitTask/UIKitTask/SecondViewController.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.navigationItem.title = "SecondVC"
         
+        self.navigationItem.title = "SecondVC"
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        perform(#selector(goBack), with: nil, afterDelay: 3.0)
+    }
+    
+    @objc func goBack() {
+        //self.navigationController?.popViewController(animated: true)
+        // Получаем текущий массив контроллеров
+        
+        var currentControllersArray = navigationController?.viewControllers
+        
+        // Удаляем контроллер
+        currentControllersArray?.removeLast()
+        
+        // Присвоим
+        if let newController = currentControllersArray {
+            navigationController?.viewControllers = newController
+        }
+    }
+    
 }
