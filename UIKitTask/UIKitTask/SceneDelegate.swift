5fb872adc28c56ed2fc38ea5a261ced8f8acdf80
Alex Schnapps <6kwalitet@gmail.com>
2023-02-16 13:44:40 +0400

    working navVC

--- UIKitTask/UIKitTask/SceneDelegate.swift
+++ UIKitTask/UIKitTask/SceneDelegate.swift
@@ -8,40 +8,46 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
         
-        let mainVC = ViewController()
-        let navController = UINavigationController(rootViewController: mainVC)
-        window?.rootViewController = navController
-        window?.backgroundColor = UIColor.white
-        window?.makeKeyAndVisible()
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            
+            let viewController = ViewController()
+            let navigationVC = UINavigationController(rootViewController: viewController)
+            
+            self.window = window
+            window.rootViewController = navigationVC
+            window.backgroundColor = UIColor.white
+            window.makeKeyAndVisible()
+        }
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
-       
+        
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
-       
+        
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
-      
+        
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
-      
+        
     }
-
-
+    
+    
 }
 
--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.title = "View Controller"
+        self.navigationItem.title = "View Controller"
     }
 
 
