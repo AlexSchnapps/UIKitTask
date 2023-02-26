0fb098e7fb911b5ba3a643f67fda8da67c41b00d
Alex Schnapps <6kwalitet@gmail.com>
2023-02-26 15:36:54 +0400

    add tabBar

--- UIKitTask/UIKitTask/SceneDelegate.swift
+++ UIKitTask/UIKitTask/SceneDelegate.swift
@@ -8,42 +8,54 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-     
+        
         guard let _ = (scene as? UIWindowScene) else { return }
         
-        self .window = UIWindow(frame: UIScreen.main.bounds)
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            let firstVC = ViewController()
+            let secondVC = secondViewController()
+            
+            //MARK: - Navigation
+            let firstNavController = UINavigationController(rootViewController: firstVC)
+            let secondNavController = UINavigationController(rootViewController: secondVC)
+            let tabBarVC = UITabBarController()
+            tabBarVC.setViewControllers([firstVC, secondVC], animated: true)
+            window.rootViewController = tabBarVC
+            window.backgroundColor = .white
+            
+            secondVC.loadViewIfNeeded()
+            
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+        }
         
-        let firstVC = ViewController()
-        let secondVC = secondViewController()
+        func sceneDidDisconnect(_ scene: UIScene) {
+            
+        }
         
-        let tabBarVC = UITabBarController()
-    }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-       
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-       
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
+        func sceneDidBecomeActive(_ scene: UIScene) {
+            
+        }
+        
+        func sceneWillResignActive(_ scene: UIScene) {
+            
+        }
+        
+        func sceneWillEnterForeground(_ scene: UIScene) {
+            
+        }
+        
+        func sceneDidEnterBackground(_ scene: UIScene) {
+            
+        }
+        
         
     }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-      
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-      
-    }
-
-
-}
-
+    
--- UIKitTask/UIKitTask/secondViewController.swift
+++ UIKitTask/UIKitTask/secondViewController.swift
@@ -12,7 +12,8 @@ class secondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.title = "SecondVC"
+    
     }
     
 
