4df6e55f7ab9b9196adce2ad44078bca3e8d7778
Alex Schnapps <6kwalitet@gmail.com>
2023-02-26 13:41:45 +0400

    end

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -14,15 +14,40 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         createImageTitleView()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(performAdd(param:)))
+        
+        let items = ["Up", "Down"]
+        let segmentController = UISegmentedControl(items: items)
+        segmentController.isMomentary = true
+        segmentController.addTarget(self, action: #selector(segmentControllerTapped(param:)), for: .valueChanged)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: segmentController)
+        
     }
     
     //MARK: - Methods
     fileprivate func createImageTitleView() {
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 40))
         imageView.contentMode = .scaleAspectFit
         let image = UIImage(named: "Ornament")
         imageView.image = image
         navigationItem.titleView = imageView
+    }
+    
+    @objc func performAdd(param: UIBarButtonItem) {
+        print("Add click")
+    }
+    
+    @objc func segmentControllerTapped(param: UISegmentedControl) {
+        switch param.selectedSegmentIndex {
+        case 0:
+            print("Up")
+        case 1:
+            print("Down")
+        default:
+            break
+        }
     }
     
 }
