f9c5270469607ca9ac08ef5330e3eb0d3892f23d
Alex Schnapps <6kwalitet@gmail.com>
2023-03-02 00:20:31 +0400

    init

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -9,8 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let myTextfield = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let textFieldFrame = CGRect(x: 0, y: 0, width: 200, height: 31)
+        myTextfield.frame = textFieldFrame
+        
+        myTextfield.center = view.center
+        view.addSubview(myTextfield)
         
     }
 
