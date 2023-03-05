b97266b82fda5c9105ab4bd02c688ee190e6df6c
Alex Schnapps <6kwalitet@gmail.com>
2023-03-05 16:37:59 +0400

    add button in InterfaceBuilder

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for button in myButtons {
+            button.setTitleColor(.green, for: .normal)
+        }
         
     }
 
 
+    @IBAction func buttonAction(_ sender: UIButton) {
+        let button = sender
+        if button.titleLabel?.text == "Button1" {
+            print("Это первая кнопка")
+        } else if button.tag == 3 {
+            print("Кнопка 3")
+        }
+    }
 }
 
