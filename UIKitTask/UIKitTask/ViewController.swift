17e5da059a55413d322488e5bdb0f00dd99ef3c5
Alex Schnapps <6kwalitet@gmail.com>
2023-03-05 21:51:38 +0400

    end

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -9,8 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var myImageView = UIImageView()
+    let anubisImage = UIImage(named: "Anubys")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myImageView = UIImageView(frame: self.view.bounds)
+        myImageView.image = anubisImage
+        myImageView.contentMode = .scaleAspectFit
+        myImageView.center = view.center
+        view.addSubview(myImageView)
         
     }
 
