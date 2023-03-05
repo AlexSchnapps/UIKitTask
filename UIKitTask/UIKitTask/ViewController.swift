3886173a4c7c8dba72a7f97cd1a7fabd1b1e98f0
Alex Schnapps <6kwalitet@gmail.com>
2023-03-05 16:47:30 +0400

    iit

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var myButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        createMyButton()
+        
     }
 
-
+//MARK: - Methods
+    private func createMyButton() {
+        myButton = UIButton(type: .roundedRect)
+        
+    }
+    
 }
 
