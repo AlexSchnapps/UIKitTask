abd63136447decc66f93889022d3956062b64788
Alex Schnapps <6kwalitet@gmail.com>
2023-03-05 18:49:41 +0400

    end

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
     var myButton = UIButton()
+    let normalImage = UIImage(named: "Ornament")
+    let highlightedImage = UIImage(named: "vintag left arrow")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +23,25 @@ class ViewController: UIViewController {
 //MARK: - Methods
     private func createMyButton() {
         myButton = UIButton(type: .roundedRect)
+        myButton.setTitle("Ornament", for: .normal)
+        myButton.setTitle("vintag left arrow", for: .highlighted)
         
+        myButton.setBackgroundImage(normalImage, for: .normal)
+        myButton.setBackgroundImage(highlightedImage, for: .highlighted)
+        
+        myButton.addTarget(self, action: #selector(buttonIsPressed(sender:)), for: .touchDown)
+        myButton.addTarget(self, action: #selector(buttonIsTapped(sender:)), for: .touchUpInside)
+        
+        myButton.frame = CGRect(x: 110, y: 200, width: 100, height: 100)
+        view.addSubview(myButton)
+    }
+    
+    @objc func buttonIsPressed(sender: UIButton) {
+        print("Button is pressed")
+    }
+    
+    @objc func buttonIsTapped(sender: UIButton) {
+        print("Button is tapped")
     }
     
 }
