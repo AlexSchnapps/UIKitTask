63ba8b048a1a6362e40e4c2a9f099e41fc0b3c86
Alex Schnapps <6kwalitet@gmail.com>
2023-03-07 17:19:52 +0400

    end

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -10,17 +10,49 @@ import UIKit
 class ViewController: UIViewController, UIScrollViewDelegate {
     
     var myScrollView = UIScrollView()
-    var myImageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
+        let ghorImage = UIImage(named: "Ghor")
+        let bhastetImage = UIImage(named: "Bhastet")
+        let anubysImage = UIImage(named: "Anubys")
+        
+        let scrollViewRect = self.view.bounds
+        
+        //CreateScrollView
+        myScrollView = UIScrollView(frame: scrollViewRect)
+        
+        // фиксация на изоброжении при перелистывании
+        myScrollView.isPagingEnabled = true
+        
+        myScrollView.contentSize = CGSize(width: scrollViewRect.size.width * 3, height: scrollViewRect.size.height)
+        self.view.addSubview(myScrollView)
+        
+        // Create image Ghor
+        var imageViewRect = self.view.bounds
+        let ghorImageView = self.newImageViewWithImage(paramImage: ghorImage!, paramFrame: imageViewRect)
+        myScrollView.addSubview(ghorImageView)
+        
+        // next page
+        imageViewRect.origin.x += imageViewRect.size.width
+        let bhastetImageView = self.newImageViewWithImage(paramImage: bhastetImage!, paramFrame: imageViewRect)
+        myScrollView.addSubview(bhastetImageView)
+        
+        // next page
+        imageViewRect.origin.x += imageViewRect.size.width
+        let anubysImageView = self.newImageViewWithImage(paramImage: anubysImage!, paramFrame: imageViewRect)
+        myScrollView.addSubview(anubysImageView)
         
     }
     //MARK: - UIScrollViewDelegate
-  
-    //MARK: - Methods
     
+    //MARK: - Methods
+    func newImageViewWithImage(paramImage: UIImage, paramFrame: CGRect) -> UIImageView {
+        let result = UIImageView(frame: paramFrame)
+        result.contentMode = .scaleAspectFit
+        result.image = paramImage
+        return result
+    }
 }
 
