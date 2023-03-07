f3a8044a2c877c8ca7ce83fdc84ea53f5cfb6d2d
Alex Schnapps <6kwalitet@gmail.com>
2023-03-08 02:10:25 +0400

    add isNetworkActivityIndicator

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController, UIWebViewDelegate {
     
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var myWebView: UIWebView!
     @IBOutlet weak var goBackItem: UIBarButtonItem!
     @IBOutlet weak var goForwardItem: UIBarButtonItem!
+    
+    let application = UIApplication.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,26 +25,40 @@ class ViewController: UIViewController, UIWebViewDelegate {
         
         //MARK: - URL/PDF
         
-        if let urlPDF = Bundle.main.url(forResource: "Matt_Neuburg_iOS_15", withExtension: "pdf") {
+      /*  if let urlPDF = Bundle.main.url(forResource: "Matt_Neuburg_iOS_15", withExtension: "pdf") {
             let request = URLRequest(url: urlPDF)
             myWebView.loadRequest(request)
-        }
+        } */
         
-     /*   if let myUrl = URL(string: "https://habr.com") {
+        if let myUrl = URL(string: "https://habr.com") {
             let request = URLRequest(url: myUrl)
             print(myUrl.port ?? myUrl, myUrl.scheme ?? myUrl, myUrl.baseURL ?? myUrl)
             myWebView.loadRequest(request)
         }
-      */
+    
+    }
+    
+    //MARK: - Method activity Animated
+    func isWorkIndicator(isAnimated: Bool, indicator: UIActivityIndicatorView) {
+        application.isNetworkActivityIndicatorVisible = isAnimated
+        if isAnimated {
+            activityIndicator.startAnimating()
+            activityIndicator.isHidden = false
+        } else {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+        }
     }
     
     //MARK: - UIWebViewDelegate
     func webViewDidStartLoad(_ webView: UIWebView) {
+        isWorkIndicator(isAnimated: true, indicator: activityIndicator)
         self.goBackItem.isEnabled = false
         self.goForwardItem.isEnabled = false
     }
     
     func webViewDidFinishLoad(_ webView: UIWebView) {
+        isWorkIndicator(isAnimated: false, indicator: activityIndicator)
         if myWebView.canGoBack {
             self.goBackItem.isEnabled = true
         } else if myWebView.canGoForward {
