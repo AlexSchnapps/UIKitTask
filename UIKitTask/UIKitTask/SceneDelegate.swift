74c044b4baae6998cf8c3a30df9f13548cad02f7
Alex Schnapps <6kwalitet@gmail.com>
2023-03-12 13:57:35 +0400

    create VC

--- UIKitTask/UIKitTask/SceneDelegate.swift
+++ UIKitTask/UIKitTask/SceneDelegate.swift
@@ -8,35 +8,41 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-     
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let vc = ViewController()
+        let navController = UINavigationController(rootViewController: vc)
+        self.window?.rootViewController = navController
+        self.window?.backgroundColor = UIColor.white
+        self.window?.makeKeyAndVisible()
+        
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
-       
+        
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
-       
+        
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
-      
+        
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
-      
+        
     }
-
-
+    
+    
 }
 
