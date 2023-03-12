313614fe44419d6baa8addbd00e2b5cb0401b972
Alex Schnapps <6kwalitet@gmail.com>
2023-03-13 00:21:37 +0400

    add constrants

---
+++ UIKitTask/UIKitTask/CarsHelper.swift
@@ -0,0 +1,13 @@
+//
+//  CarsHelper.swift
+//  UIKitTask
+//
+//  Created by Alex Schnapps on 12.03.2023.
+//
+
+import UIKit
+
+struct CarsHelper {
+    var name = String()
+    var image = UIImage()
+}
--- UIKitTask/UIKitTask/VC/CarViewController.swift
+++ UIKitTask/UIKitTask/VC/CarViewController.swift
@@ -8,22 +8,65 @@
 import UIKit
 
 class CarViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    //MARK: - Image
+    private let carImage: UIImageView = {
+        let view = UIImageView()
+        view.layer.cornerRadius = 20
+        view.layer.masksToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    //MARK: - label
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 30)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    //MARK: - Lazy varibal
+    lazy var subView: [UIView] = [self.carImage, self.nameLabel]
+    
+    //MARK: - Init
+    init(carWith: CarsHelper) {
+        super.init(nibName: nil, bundle: nil)
+        view.backgroundColor = .purple
+        edgesForExtendedLayout = []
+        
+        carImage.image = carWith.image
+        nameLabel.text = carWith.name
+        
+        for view in subView {self.view.addSubview(view)}
+        //MARK: - Constraints
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: carImage, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 250),
+        
+            NSLayoutConstraint(item: carImage, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 250),
+            
+            NSLayoutConstraint(item: carImage, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 150),
+            
+            NSLayoutConstraint(item: carImage, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0)
+            
+        ])
+        
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: nameLabel, attribute: .top, relatedBy: .equal, toItem: carImage, attribute: .bottom, multiplier: 1, constant: 50),
+        
+            NSLayoutConstraint(item: nameLabel, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0)
+        ])
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
 }
