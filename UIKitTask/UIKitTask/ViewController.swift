d291ff89d5f54f74ac870f1fa4c2ef81fe737887
Alex Schnapps <6kwalitet@gmail.com>
2023-03-13 21:14:24 +0400

    create animation and gravity square

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //UICollisionBehavior - поведение при столкновении
+    //UIGravityBehavior - обеспечивает гравитацию
+    //UIPushBehavior - реакции на толчки
+    //UISnapBehavior - крепит view к определенной точке
 
+    let squareView = UIView()
+    var animator = UIDynamicAnimator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        //MARK: - create a square (квадрат)
+        squareView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        squareView.backgroundColor = .green
+        squareView.center = view.center
+        view.addSubview(squareView)
+        
+        //MARK: - create animation and gravity
+        animator = UIDynamicAnimator(referenceView: view)
+        let gravity = UIGravityBehavior(items: [squareView])
+        animator.addBehavior(gravity)
+    }
 
+    
+    
 }
 
