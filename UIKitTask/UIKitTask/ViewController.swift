fb76bfbb2ccda254777ba5c4e59b45084d3c024d
Alex Schnapps <6kwalitet@gmail.com>
2023-03-14 18:29:58 +0400

    end

--- UIKitTask/UIKitTask/ViewController.swift
+++ UIKitTask/UIKitTask/ViewController.swift
@@ -9,9 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // UISnapBehavior
+    
+    var squareView = UIView()
+    var animator = UIDynamicAnimator()
+    var snapBehavior: UISnapBehavior?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        createGastureRecognizer()
+        createSmallSquareView()
+        createAnimatorAndBehaviors()
+    }
+    
+    func createGastureRecognizer() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(paramTap:)))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func createSmallSquareView() {
+        squareView = UIView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
+        squareView.backgroundColor = .green
+        squareView.center = view.center
+        view.addSubview(squareView)
+    }
+    
+    func createAnimatorAndBehaviors() {
+        animator = UIDynamicAnimator(referenceView: view)
+        let collision = UICollisionBehavior(items: [squareView])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
+        
+        // зацеп
+        snapBehavior = UISnapBehavior(item: squareView, snapTo: squareView.center)
+        snapBehavior?.damping = 0.5
+        animator.addBehavior(snapBehavior!)
+    }
+    
+    @objc func handleTap(paramTap: UITapGestureRecognizer) {
+        // получакм угол между центром квадрата и точкой касания
+        let tapPoint = paramTap.location(in: view)
+        if snapBehavior != nil {
+            animator.removeBehavior(snapBehavior!)
+        }
+        snapBehavior = UISnapBehavior(item: squareView, snapTo: tapPoint)
+        snapBehavior?.damping = 0.5 // Средняя щсциляция
+        animator.addBehavior(snapBehavior!)
     }
     
 }
